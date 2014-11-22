fa12bfdd29fe5bbfc9eacd9ce79ab10e64718f50
nkniazi <alnasrkhan@gmail.com>
2014-11-22 17:27:50 +0500

    two

--- SensorRawAccelerometer1/src/com/example/sensorrawaccelerometer1/MainActivity.java
+++ SensorRawAccelerometer1/src/com/example/sensorrawaccelerometer1/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.sensorrawaccelerometer1;
 
 import android.app.Activity;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private TextView mXValueView;
+	private TextView mYValueView;
+	private TextView mZValueView;
+	private SensorManager mSensorManager;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mXValueView=(TextView)findViewById(R.id.x_data);
+        mYValueView=(TextView)findViewById(R.id.y_data);
+        mZValueView=(TextView)findViewById(R.id.z_data);
+        mSensorManager=(SensorManager)getSystemService(SEARCH_SERVICE);
+        
+        
     }
 }
